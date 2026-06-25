a28ee9fa234ef76e6118b166fb74f6fa46872fde
Yebung-i <lantern1095@gmail.com>
2026-06-25 14:10:35 +0900

    Update D2Task2.py

--- Day2/Task2/D2Task2.py
+++ Day2/Task2/D2Task2.py
@@ -3,10 +3,11 @@ import numpy as np
 A = np.array([[1,2,3],[-1,3,-10]])
 B = np.array([[2,-1],[3,1],[0,5]])
 
+AB = np.zeros((A.shape[0], B.shape[1]))
 
-AB = np.zeros(A.shape[0], B.shape[1])
+for i in range(A.shape[0]):        # A의 행
+    for j in range(B.shape[1]):    # B의 열
+        for k in range(A.shape[1]):# 공통 차원
+            AB[i, j] += A[i, k] * B[k, j]
 
-for i in range(A.shape[0]):
-    for j in range(B.shape[1]):
-        for k in range(A.shape[1]):
-            AB[i][j] += A[i][k] * B[k][j]
+print(AB)
