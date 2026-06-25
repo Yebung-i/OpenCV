0778644e1895938ef9f9ef2e2f5c8e6b4f7863d5
Yebung-i <lantern1095@gmail.com>
2026-06-25 14:10:51 +0900

    Update D2Task2.py

--- Day2/Task2/D2Task2.py
+++ Day2/Task2/D2Task2.py
@@ -5,9 +5,9 @@ B = np.array([[2,-1],[3,1],[0,5]])
 
 AB = np.zeros((A.shape[0], B.shape[1]))
 
-for i in range(A.shape[0]):        # A의 행
-    for j in range(B.shape[1]):    # B의 열
-        for k in range(A.shape[1]):# 공통 차원
+for i in range(A.shape[0]):      
+    for j in range(B.shape[1]):  
+        for k in range(A.shape[1]):
             AB[i, j] += A[i, k] * B[k, j]
 
 print(AB)
