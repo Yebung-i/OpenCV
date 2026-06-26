d37938f8dcd94dc30fa0599ade0e292e02a35d0e
Yebung-i <lantern1095@gmail.com>
2026-06-26 14:00:57 +0900

    Update D3Task3.py

--- Day3/Task2/D3Task3.py
+++ Day3/Task2/D3Task3.py
@@ -1,1 +1,16 @@
-## 실행결과
+import cv2
+import numpy as np
+
+img = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
+if img is None:
+    raise Exception("파일 읽기 오류")
+
+def onMouse(event, x, y, flage, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(f"좌표: ({x},{y}), 화소값: {img[y,x]}")
+
+cv2.imshow('img', img)
+cv2.setMouseCallback('img', onMouse)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
