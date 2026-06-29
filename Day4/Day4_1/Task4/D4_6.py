3ed5a9c79c31da9059a28c4af95178963a430ff5
Yebung-i <lantern1095@gmail.com>
2026-06-29 14:06:43 +0900

    Update D4_6.py

--- Day4/Day4_1/Task4/D4_6.py
+++ Day4/Day4_1/Task4/D4_6.py
@@ -1,1 +1,43 @@
 
+import numpy as np
+import cv2
+import time
+
+image = np.zeros((300,300), np.uint8)
+
+count = 0
+isCounting = False
+LastTime = time.time()
+
+
+while True:
+    image.fill(255)
+
+    if isCounting:
+        curTime = time.time()
+
+        if curTime - LastTime >= 1.0:
+            count += 1
+            LastTime = curTime
+
+    cv2.putText(image, str(count), (110, 190), cv2.FONT_HERSHEY_SIMPLEX, 4, 0, 5)
+    cv2.imshow(' TimeCount ', image)
+
+    key = cv2.waitKey(10)
+
+    if key == ord('s'):
+        if not isCounting:
+            LastTime = time.time()
+            isCounting = True
+
+    elif key == ord('t'):
+        isCounting = False
+
+    elif key == ord('r'):
+        isCounting = False
+        count = 0
+
+    elif key == ord ('q'):
+        break;
+
+cv2.destroyAllWindows()
