b05e134b4fdb50363e782fdb24a0184394c347f8
Yebung-i <lantern1095@gmail.com>
2026-06-29 11:44:56 +0900

    Update D4_6.py

--- Day4/Day4_1/Task4/D4_6.py
+++ Day4/Day4_1/Task4/D4_6.py
@@ -4,10 +4,8 @@ import cv2
 image = np.zeros((300, 300), np.uint8)
 black = 0
 count = 0
+isCounting = False
 
-for i in range(count, 9):
-    cv2.putText(image, count, (100, 150), cv2.FONT_HERSHEY_SIMPLEX, 4, black)
-    cv2.imshow(' img ', image)
-    cv2.waitKey(1000)
-cv2.destroyAllWindows()
-
+while True:
+    if isCounting:
+    
