b94c7eba3af9df1309ae459bbfdc4037a69639d8
Yebung-i <lantern1095@gmail.com>
2026-06-30 14:35:57 +0900

    Update D4_9.py

--- Day4/Day4_1/Task7/D4_9.py
+++ Day4/Day4_1/Task7/D4_9.py
@@ -10,26 +10,52 @@ bar_name = "Color"
 isClicked = False
 prevLocation = (0,0)
 
+colorDic = {
+    "blue" : (255,0,0),
+    "green" : (0,255,0),
+    "red" : (0,0,255)
+}
+
+def PASS(x):
+    pass
+
 def getColor():
-    if color == 0:
-        return (255,0,0)
-    elif color ==
+
+    pos = cv2.getTrackbarPos(bar_name, title) # 트랙바 이름, 이미지 이름으로 해당하는 트랙바의 값을 찾아옴
+
+    if pos == 0:
+        return colorDic["blue"]
+    elif pos == 1:
+        return colorDic["green"]
+    elif pos == 2:
+        return colorDic["red"]
 
 
 def clickMouse(event, x, y, flage, param):
+    global isClicked, prevLocation
+
     if event == cv2.EVENT_LBUTTONDOWN:
         if not isClicked:
             prevLocation = (x,y)
             isClicked = True
-        elif event == cv2.EVENT_LBUTTONUP:
-            if isClicked:
-                cv2.line(image, prevLocation, (x,y), getColor(), 2)
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if isClicked:
+            color = getColor()
+            cv2.line(image, prevLocation, (x,y), getColor(), 2)
+            cv2.imshow(title, image)
+            prevLocation = (x,y)
+
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        if isClicked:
+            isClicked = False
 
 cv2.imshow(title, image)
-cv2.createTrackbar("Color", title, 0,2)
 cv2.setMouseCallback(title, clickMouse)
+cv2.createTrackbar("Color", title, 0,2, PASS)
 
 while True:
-    if cv2.waitKey(1) : break
+    if cv2.waitKey(1) >= 0 : break
 
 cv2.destroyAllWindows()
