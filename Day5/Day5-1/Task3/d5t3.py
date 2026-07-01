d7eabb470aa15749798fcdbad11c663fd56dc887
Yebung-i <lantern1095@gmail.com>
2026-07-01 10:48:08 +0900

    Update d5t3.py

--- Day5/Day5-1/Task3/d5t3.py
+++ Day5/Day5-1/Task3/d5t3.py
@@ -1,1 +1,38 @@
+import numpy as np
+import cv2
 
+def onChange(value):
+    global image, title, isAdd
+
+    if value == 0:
+        if isAdd == True:
+            isAdd = False
+    
+    else:
+        if isAdd == False:
+            isAdd = True
+
+def onMouse(event, x, y, flage, param):
+    global image, title, isAdd
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if isAdd == True:
+            image = cv2.add(image, 10)
+            cv2.imshow(title, image)
+        else:
+            image = cv2.subtract(image, 10)
+            cv2.imshow(title, image)    
+
+
+    
+
+image = cv2.imread("lenna.bmp", cv2.IMREAD_GRAYSCALE)
+if image is None : raise Exception("파일 읽기 오류")
+
+isAdd = False
+title = "image"
+
+cv2.imshow(title, image)
+cv2.createTrackbar("Brightness", title, 0, 1, onChange)
+cv2.setMouseCallback(title, onMouse)
+cv2.waitKey(0)
